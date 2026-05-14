d510cd41ba7e3d344171970b2ce8038a62d931de
Supver333 <157446034+Supver333@users.noreply.github.com>
2026-05-15 02:17:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Hihi Dành Tặng Người Yêu Tớ Nè",
+    pageTitle: "for-my-love",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
